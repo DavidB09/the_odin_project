64f7fbbbae1472466612f6a09037fcb4d4c4efbf
David <davcoding1@gmail.com>
2021-08-10 17:23:02 -0500

    Deleted old logic for calculator project

--- foundations/calculator/script.js
+++ foundations/calculator/script.js
@@ -5,120 +5,110 @@ const valueElem = document.querySelector('.main-value');
 const operatorElems = document.querySelectorAll('.operator'); 
 const numberElems = document.querySelectorAll('.number'); 
 
+const decimalBtn = document.querySelector('.decimal'); 
+const equalBtn = document.querySelector('.equal'); 
+
 const undoBtn = document.querySelector('.undo'); 
 const clearBtn = document.querySelector('.clear');
 
-let currEquation = ''; 
-let currValue = ''; 
-let isNew = false; 
-
+let operand1 = ''; 
+let operand2 = '';
+let operator = ''; 
+let currInput = ''; 
 
 const updateCalculator = () => {
-    equationElem.textContent = currEquation; 
-    valueElem.textContent = currValue; 
+    equationElem.textContent = `${operand1} ${operator} ${operand2}`.trim(); 
+    valueElem.textContent = currInput; 
 }; 
 
+const runCalculation = () => {
+    let total = 0; 
+
+    switch(operator) {
+        case '*': 
+            total = Number(operand1) * Number(operand2); 
+            break; 
+        case '/':
+            total = Number(operand1) / Number(operand2); 
+            break;
+        case '+':
+            total = Number(operand1) + Number(operand2); 
+            break;
+        case '-':
+            total = Number(operand1) - Number(operand2);
+            break;  
+    }; 
+
+    operand1 = `${total}`; 
+    operand2 = ''; 
+    operator = ''; 
+    currInput = operand1; 
+}; 
+
+const handleOperator = (value) => {
+    if (operand1 && operand2) runCalculation(); 
+
+    operator = value; 
+    updateCalculator(); 
+}; 
+
+const handleEqual = () => {
+    if (operand1 && operand2) runCalculation(); 
+    if (!/[=]/g.test(equationElem.textContent)) equationElem.textContent += ` = ${currInput}`; 
+    valueElem.textContent = currInput; 
+}; 
+
+operatorElems.forEach(operator => operator.addEventListener('click', () => handleOperator(operator.value))); 
+equalBtn.addEventListener('click', handleEqual); 
+
+const handleOperand = () => {
+    if (!operator) operand1 = currInput; 
+    else operand2 = currInput; 
+}; 
+
+const handleNumber = (value) => {
+    if (operand1 && operator && !operand2) currInput = ''; 
+    if (currInput === '0') currInput =  ''; 
+
+    currInput += value; 
+    handleOperand(); 
+    updateCalculator(); 
+}; 
+
+const handleDecimal = () => {
+    if (operand1 && operator && !operand2) currInput = ''; 
+    if (/[.]/g.test(currInput)) return; 
+
+    if (!currInput) currInput = '0.'; 
+    else currInput += '.'; 
+    handleOperand();
+    updateCalculator(); 
+}; 
+
+numberElems.forEach(number => number.addEventListener('click', () => handleNumber(number.value))); 
+decimalBtn.addEventListener('click', handleDecimal); 
+
 const clearCalculator = () => {
-    currEquation = ''; 
-    currValue = '0'; 
+    operand1 = '0'; 
+    operand2 = '';
+    operator = ''; 
+    currInput = '0'; 
     updateCalculator(); 
 }; 
 
 const undoCalculator = () => {
-    currValue = currValue.slice(0, currValue.length - 1); 
+    if (operand2) {
+        operand2 = operand2.slice(0, operand2.length - 1);
+        currInput = operand2; 
+    } else if (operator) {
+        operator = ''; 
+        currInput = operand1; 
+    } else if (operand1) {
+        operand1 = operand1.slice(0, operand1.length - 1);
+        currInput = operand1; 
+    }
     updateCalculator(); 
 }; 
-
-const updateNewCalculation = () => {
-    if (isNew) {
-        currEquation = `${currValue}`; 
-        isNew = false; 
-    }
-}; 
-
-const runCalculation = () => {
-    let [num1, op, num2] = currEquation.split(' '); 
-    let total = 0; 
-
-    if (!num1 || !op || !num2) return 'NOT ALLOWED'; 
-
-    switch(op) {
-        case '*': 
-            total = Number(num1) * Number(num2); 
-            break; 
-        case '/':
-            total = Number(num1) / Number(num2); 
-            break;
-        case '+':
-            total = Number(num1) + Number(num2); 
-            break;
-        case '-':
-            total = Number(num1) - Number(num2);
-            break;  
-    }
-
-    return total; 
-}
-
-const handleOperator = (operator) => {
-    if (isNew) updateNewCalculation(); 
-
-    let currTotal = runCalculation(); 
-
-    if (currTotal === 'NOT ALLOWED' && operator === '=') return; 
-
-    if (currTotal !== 'NOT ALLOWED') {
-        if (operator === '=') {
-            currEquation += ` ${operator} ${currTotal}`; 
-        } else {
-            currEquation = currTotal; 
-        }
-        currValue = currTotal; 
-        updateCalculator(); 
-        isNew = true; 
-        return; 
-    }
-
-    if (/[*/+-] $/.test(currEquation)) {
-        currEquation = currEquation.slice(0, -2); 
-    }
-
-    currEquation += ` ${operator} `; 
-    updateCalculator(); 
-}; 
-
-operatorElems.forEach(operator => {
-    operator.addEventListener('click', () => handleOperator(operator.value)); 
-})
-
-const handleNumber = (number) => {
-    if (isNew) updateNewCalculation(); 
-
-    if (/[*/+-] $/.test(currEquation)) currValue = ''; 
-
-    if (currValue === '0') {
-        currValue = (number === '.' ? currValue : ''); 
-        currEquation = currEquation.slice(0, (number === '.' ? currEquation.length : -1)); 
-    }
-
-    if (number === '.') {
-        if (/[.]/g.test(currValue)) return; 
-        if (!currValue) {
-            currValue = '0'; 
-            currEquation += '0'; 
-        }
-    }
-
-    currValue += number; 
-    currEquation += number; 
-    updateCalculator(); 
-}; 
-
-
-numberElems.forEach(number => {
-    number.addEventListener('click', () => handleNumber(number.value)); 
-}); 
-
 
 clearBtn.addEventListener('click', clearCalculator);
 undoBtn.addEventListener('click', undoCalculator); 
