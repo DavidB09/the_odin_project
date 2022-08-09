0a26fbd6c585fda481c379817fcc6324fe308831
DavidB09 <davcoding1@gmail.com>
2022-08-08 22:24:08 -0500

    Update styling and images for front page

--- javascript/restaurant/src/script.js
+++ javascript/restaurant/src/script.js
@@ -1,1 +1,17 @@
-import './style.scss'
+import './style.scss';
+
+import pizzaSvg from './assets/svg/pizza.svg';
+import uberSvg from './assets/svg/uber-eats.svg';
+import grubhubSvg from './assets/svg/grubhub.svg';
+
+
+(function createIMG() {
+    const pizzaImg = document.querySelector('img.pizza');
+    pizzaImg.setAttribute('src', pizzaSvg);
+
+    const uberImg = document.querySelector('img.uber');
+    uberImg.setAttribute('src', uberSvg);
+
+    const grubhubImg = document.querySelector('img.grubhub');
+    grubhubImg.setAttribute('src', grubhubSvg);
+})();
